1055fe9ca1ab8b8c8639100f826fb45c510067f9
Totskiy <utkinsasha228@gmail.com>
2024-10-14 01:12:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ onload = function () {
         return response.json();
     })
     .then(data => {
-        if (data['online']) {
+        if (!data['online']) {
             statusObjText.innerText = 'Offline';
             statusObj.style.color = "#ff9f9f"
         } else {
